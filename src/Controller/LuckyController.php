388f5e129f4bbdf0beb0bde207f86ad0da2983de
Dima <dmd29.dima@gmail.com>
2018-03-25 15:12:56 +0300

    first page

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skillup_student
+ * Date: 25.03.18
+ * Time: 15:02
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+
+    /**
+     * @Route("/lucky/number")
+     *
+     * @return Response
+     */
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+}
